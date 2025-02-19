a216a2cdb544eaaffea51000fa05ffe05ff26c7a
divyanshusingh <divyanshusingh1101@gmail.com>
2025-02-19 20:14:42 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { RequestAirdrop } from '../Airdrop';
+import React, { useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from '@solana/web3.js';
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+
+function App() {
+ const network = WalletAdapterNetwork.Devnet;
+ const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+ const wallets= useMemo(()=> [new PhantomWalletAdapter()], []);
+ 
+  return (
+    <ConnectionProvider endpoint= "https://api.devnet.solana.com">
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton/>
+          <WalletDisconnectButton/>
+            <RequestAirdrop/>{/*for app component  */}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App
